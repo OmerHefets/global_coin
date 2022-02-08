09ebdf6045ff05a67097d99650430aa83acde299
Omer Hefets <hefets.omer@gmail.com>
2022-02-08 20:48:21 +0200

    added Block class getters and setters with unittests

--- blockchain/node/block.py
+++ blockchain/node/block.py
@@ -1,12 +1,15 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from exceptions import BlockException
 
-HASH_LENGTH = 64 # hash is 32 bytes
+HASH_LENGTH = 64  # hash is 32 bytes
+TIMESTAMP_DELTA_VALIDATION = 1  # 1 minute delay in a block processing
+
 
 class Block:
-    def __init__(self, hash: str,
+    def __init__(self,
+                 hash: str,
                  prev_block_hash: str,
-                 merkle_root: str, 
+                 merkle_root: str,
                  timestamp: datetime,
                  difficulty: float,
                  nonce: int,
@@ -20,24 +23,88 @@ class Block:
         self.height = height
 
     @property
-    def hash(self):
+    def hash(self) -> str:
         return self._hash
 
     @hash.setter
-    def hash(self, new_hash):
+    def hash(self, new_hash) -> None:
         if not self.hash_validation(new_hash):
-            raise BlockException("Invalid hash value.")
+            raise BlockException("Invalid hash value")
         self._hash = new_hash
+
+    @property
+    def prev_block_hash(self) -> str:
+        return self._prev_block_hash
+
+    @prev_block_hash.setter
+    def prev_block_hash(self, new_hash) -> None:
+        if not self.hash_validation(new_hash):
+            raise BlockException("Invalid prev hash value")
+        self._prev_block_hash = new_hash
+
+    @property
+    def merkle_root(self) -> str:
+        return self._merkle_root
+
+    @merkle_root.setter
+    def merkle_root(self, new_root) -> None:
+        self._merkle_root = new_root
+
+    @property
+    def timestamp(self) -> float:
+        return self._timestamp
+
+    @timestamp.setter
+    def timestamp(self, new_timestamp) -> None:
+        if not self.timestamp_validation(new_timestamp):
+            raise BlockException("Invalid timestamp value")
+        self._timestamp = new_timestamp
+
+    # insert difficulty
+
+    @property
+    def nonce(self) -> int:
+        return self._nonce
+
+    @nonce.setter
+    def nonce(self, new_nonce) -> None:
+        if not self.nonce_validation(new_nonce):
+            raise BlockException("Invalid nonce value")
+        self._nonce = new_nonce
+
+    @property
+    def height(self) -> int:
+        return self._height
+
+    @height.setter
+    def height(self, new_height) -> None:
+        if not self.height_validation(new_height):
+            raise BlockException("Invalid height value")
+        self._height = new_height
 
     @staticmethod
     def hash_validation(hash) -> bool:
-        if not isinstance(hash, str) or len(hash) < 2:
+        if not isinstance(hash, str) or len(hash) < HASH_LENGTH:
             return False
         else:
             return True
-        
 
-# b = Block("123", "234", "4323", datetime(year=2020, month=11, day=12), 1.23, 10, 1)
-# print(b)
-# if type(5) == int:
-#    raise BlockException("WoW")
+    @staticmethod
+    def timestamp_validation(timestamp) -> bool:
+        current_timestamp_with_delta = datetime.timestamp(datetime.now() + timedelta(minutes=TIMESTAMP_DELTA_VALIDATION))
+        if not isinstance(timestamp, float) or timestamp > current_timestamp_with_delta:
+            return False
+        else:
+            return True
+
+    @staticmethod
+    def nonce_validation(nonce) -> bool:
+        return False if not isinstance(nonce, int) else True
+
+    @staticmethod
+    def difficulty_validation(difficulty) -> bool:
+        return False if not isinstance(difficulty, float) else True
+
+    @staticmethod
+    def height_validation(height) -> bool:
+        return False if not isinstance(height, int) else True
--- blockchain/node/test_block.py
+++ blockchain/node/test_block.py
@@ -1,40 +1,64 @@
 import pytest
-from datetime import datetime
+from datetime import datetime, timedelta
 from exceptions import BlockException
 from block import Block
 
 
 @pytest.fixture
-def test_block():
+def test_block() -> Block:
     return Block(hash="00000000000000027e7ba6fe7bad39faf3b5a83daed765f05f7d1b71a1632249",
                  prev_block_hash="00000000000000027e7ba6fe7bad39faf3b5a83daed765f05f7d173951632249",
                  merkle_root="5e049f4030e0ab2debb92378f53c0a6e09548aea083f3ab25e1d94ea1155e29d",
-                 timestamp=datetime.timestamp(datetime(year=2022, month=11, day=26)),
+                 timestamp=datetime.timestamp(
+                     datetime(year=2021, month=11, day=26)),
                  difficulty=180923195.25802612,
                  nonce=4215469401,
                  height=124193)
 
 
 @pytest.mark.parametrize(argnames="new_hash",
-                         argvalues=[("343525"),
-                                    ("abc483293"),
-                                    ("blabla"),
+                         argvalues=[("00000000000000027e7ba6fe7bad39faf3b5a83daed765f05f7d1b71a1632255"),
+                                    ("00000000000000027E7BA6FE7BAD39FAF3B5A83DAED765F05F7D1B71A1632249"),
+                                    ("00000000000000027E7BA6fe7bad39fAf3B5a83DAed765f05f7d1b71a1632249"),
                                     ])
 def test_set_valid_hash(test_block, new_hash):
+    """
+    A valid hash is in the format of: 64 characters, HEXA only, string only
+
+    """
     test_block.hash = new_hash
     assert test_block.hash == new_hash
 
 
 @pytest.mark.parametrize(argnames="new_hash",
                          argvalues=[(12345),
-                                    (1.323), 
-                                    ([1, 2, 3]), 
+                                    (1.323),
+                                    ([1, 2, 3]),
                                     ({"hash": 34555}),
-                                    ("2")
+                                    ("000027E7BA6fe7bad39fAf3B5a83DAed765f05f7d1b71a1632249")
                                     ])
 def test_set_invalid_hash(test_block, new_hash):
     with pytest.raises(BlockException):
         test_block.hash = new_hash
 
-def test_timestamp(test_block):
-    print(test_block.timestamp)
+
+@pytest.mark.parametrize(argnames="new_timestamp",
+                         argvalues=[(datetime.timestamp(datetime.now())),
+                                    (datetime.timestamp(
+                                        datetime.now() - timedelta(minutes=5))),
+                                    (datetime.timestamp(
+                                        datetime.now() + timedelta(seconds=30)))
+                                    ])
+def test_set_valid_timestamp(test_block, new_timestamp):
+    test_block.timestamp = new_timestamp
+    assert test_block.timestamp == new_timestamp
+
+
+@pytest.mark.parametrize(argnames="new_timestamp",
+                         argvalues=[(datetime(year=2021, month=10, day=10)),
+                                    (datetime.timestamp(datetime.now() + timedelta(minutes=2))),
+                                    (str(datetime.timestamp(datetime.now())))
+                                    ])
+def test_set_invalid_timestamp(test_block, new_timestamp):
+    with pytest.raises(BlockException):
+        test_block.timestamp = new_timestamp
