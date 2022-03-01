c88ca6ef9326bfb1a4656f606047a43423c2e3f5
Omer Hefets <hefets.omer@gmail.com>
2022-03-02 00:10:51 +0200

    completed tx db CRUD functionality

--- blockchain/node/dal/blockchain_tx_db/tx_data_manager_interface.py
+++ blockchain/node/dal/blockchain_tx_db/tx_data_manager_interface.py
@@ -10,24 +10,24 @@ from bl.transaction import Transaction
 class NodeTransactionInterface(abc.ABC):
     @classmethod
     def __subclasshook__(cls, subclass):
-        return (hasattr(subclass, 'get_tx_by_hash') and
-        callable(subclass.get_tx_by_hash) and
-        hasattr(subclass, 'get_tx_by_block_hash') and
-        callable(subclass.get_tx_by_block_hash) and
+        return (hasattr(subclass, 'get_tx_by_txid') and
+        callable(subclass.get_tx_by_txid) and
+        hasattr(subclass, 'get_txs_by_block_hash') and
+        callable(subclass.get_txs_by_block_hash) and
         hasattr(subclass, 'set_new_tx') and
         callable(subclass.set_new_tx) and
-        hasattr(subclass, 'update_tx_by_hash') and
-        callable(subclass.update_tx_by_hash) and
-        hasattr(subclass, 'delete_tx_by_hash') and
-        callable(subclass.delete_tx_by_hash) or
+        hasattr(subclass, 'update_tx_by_txid') and
+        callable(subclass.update_tx_by_txid) and
+        hasattr(subclass, 'delete_tx_by_txid') and
+        callable(subclass.delete_tx_by_txid) or
         NotImplemented)
 
     @abc.abstractclassmethod
-    def get_tx_by_hash(self, hash: str) -> Dict:
+    def get_tx_by_txid(self, txid: str) -> Dict:
         raise NotImplementedError
 
     @abc.abstractclassmethod
-    def get_tx_by_block_hash(self, block_hash: str) -> Dict:
+    def get_txs_by_block_hash(self, block_hash: str) -> Dict:
         raise NotImplementedError
 
     @abc.abstractclassmethod
@@ -35,9 +35,9 @@ class NodeTransactionInterface(abc.ABC):
         raise NotImplementedError
 
     @abc.abstractclassmethod
-    def update_tx_by_hash(self, hash: str, tx: Transaction) -> None:
+    def update_tx_by_txid(self, txid: str, tx: Transaction) -> None:
         raise NotImplementedError
 
     @abc.abstractclassmethod
-    def delete_tx_by_hash(self, hash: str) -> None:
+    def delete_tx_by_txid(self, txid: str) -> None:
         raise NotImplementedError
--- blockchain/node/dal/blockchain_tx_db/tx_data_manager_sql.py
+++ blockchain/node/dal/blockchain_tx_db/tx_data_manager_sql.py
@@ -1,8 +1,12 @@
+from re import T
 import sys
 import os
+
+from numpy import block
+from pyparsing import dbl_slash_comment
 sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
 
-from typing import Dict
+from typing import Dict, List
 from dal.blockchain_tx_db.tx_data_manager_interface import NodeTransactionInterface
 from bl.transaction import Transaction
 from dal.sql_database_connection import DatabaseConnection
@@ -13,24 +17,90 @@ class TransactionDataManager(NodeTransactionInterface):
         self.db_connection = DatabaseConnection()
 
 
-    def get_tx_by_hash(self, hash: str) -> Dict:
+    def get_tx_by_txid(self, txid: str) -> Dict:
         self.db_connection.cursor.execute(
             """ SELECT * FROM node_transactions WHERE txid=%s""",
-            vars=(hash,)
+            vars=(txid,)
         )
 
         tx = dict(self.db_connection.cursor.fetchone())
 
         return tx
 
-    def get_tx_by_block_hash(self, block_hash: str) -> Dict:
-        pass
+    def get_txs_by_block_hash(self, block_hash: str) -> Dict:
+        self.db_connection.cursor.execute(
+            """ SELECT * FROM node_transactions WHERE tx_block_hash=%s""",
+            vars=(block_hash,)
+        )
+
+        tx_list = list(self.db_connection.cursor.fetchall())
+        tx_list_of_dicts: List[Dict] = [dict(tx) for tx in tx_list]
+
+        return tx_list_of_dicts
 
     def set_new_tx(self, tx: Transaction) -> None:
-        pass
+        self.db_connection.cursor.execute(
+            """ INSERT INTO node_transactions
+            (txid, tx_block_hash, tx_block_index, vin, 
+            vout_addr, vout_value, vout_script,
+            vchange_addr, vchange_value, vchange_script)
 
-    def update_tx_by_hash(self, hash: str, tx: Transaction) -> None:
-        pass
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+            """,
+            vars=(tx.txid, tx.tx_block_hash, str(tx.tx_block_index), tx.encode_tx_vin(), 
+            tx.vout_addr, str(tx.vout_value), tx.vout_script,
+            tx.vchange_addr, str(tx.vchange_value), tx.vchange_script) 
+        )
 
-    def delete_tx_by_hash(self, hash: str) -> None:
-        pass
+        self.db_connection.conn.commit()
+
+    def update_tx_by_txid(self, txid: str, tx: Transaction) -> None:
+        self.db_connection.cursor.execute(
+            """ UPDATE node_transactions SET
+            txid = %s, tx_block_hash = %s, tx_block_index = %s, vin = %s, 
+            vout_addr = %s, vout_value = %s, vout_script = %s, 
+            vchange_addr = %s, vchange_value = %s, vchange_script = %s
+            
+            WHERE txid = %s""",
+            vars=(tx.txid, tx.tx_block_hash, str(tx.tx_block_index), tx.encode_tx_vin(),
+            tx.vout_addr, str(tx.vout_value), tx.vout_script, 
+            tx.vchange_addr, str(tx.vchange_value), tx.vout_script, txid)
+        )
+
+        self.db_connection.conn.commit()
+
+    def delete_tx_by_txid(self, txid: str) -> None:
+        self.db_connection.cursor.execute(
+            """ DELETE FROM node_transactions WHERE txid = %s""",
+            vars=(txid,)
+        )
+        
+        self.db_connection.conn.commit()
+
+tdm = TransactionDataManager()
+tx_dict = tdm.get_tx_by_txid(txid='a1e1e9761e5fde1dfc626297ff71deea569b6a61fa7e9f9797dcfffa662c381a')
+
+print(tdm.get_txs_by_block_hash(block_hash='00000000000000027e7ba6fe7bad39faf3b5a83daed765f05f7d1b71a1632249'))
+
+vin = [{
+        "vin_addr": "1VayNert3x1KzbpzMGt2qdqrAThiRovi8",
+        "vin_value": 627907074,
+        "vin_script": "3046022100cf19e206eb882624d9631a443eaf4925894" + \
+            "3040e9c680bf054881e548606ee77022100a1d624adf36015bfb772171046b" + \
+            "1aa2edbed7c1fd20ec8c57fabaaebf0312bed01"
+    }]
+
+tx = Transaction(tx_block_hash="543bd63267bb4d736377e66666666666666666",
+                       tx_block_index=4,
+                       vin=vin,
+                       vout_addr="1BpqjnfKs1akUzzqxAEW6dVBU",
+                       vout_value=90000000,
+                       vout_script="76bd7e03393ceda9815b392e5bab45b330",
+                       vchange_addr="1VayNerGt2qdqrAThiRovi8",
+                       vchange_value=537907074,
+                       vchange_script="04a39b9e4fbd213ef23d04e763bdc5a071c0e827c0bd834a5")
+
+
+tdm.update_tx_by_txid(txid='7c084390791c6bb1d39ebb861d072b5cdb075b3fda7344f22cf8b5b43603b40d', tx=tx)
+
+tdm.delete_tx_by_txid(txid='7c084390791c6bb1d39ebb861d072b5cdb075b3fda7344f22cf8b5b43603b40d')
